2408e6f8699325ff5ca3f4a57cf37d4194fb41b9
John Lima <john.limadeveloper@gmail.com>
2016-11-27 21:14:48 -0200

    • Global;

--- Timer/TimerWatch Extension/InterfaceController.swift
+++ Timer/TimerWatch Extension/InterfaceController.swift
@@ -14,16 +14,21 @@ class InterfaceController: WKInterfaceController {
     // MARK: - Properties
     @IBOutlet private weak var timer: WKInterfaceTimer!
     
+    private var doneTimer: Timer?
+    private var duration: TimeInterval = 60 * 3 // number of seconds (30 seconds)
+    
     // MARK: - View LifeCycle
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
         // Configure interface objects here.
+        
+        self.timer.setDate(Date(timeIntervalSinceNow: self.duration+1))
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
     }
     
     override func didDeactivate() {
@@ -35,7 +40,15 @@ class InterfaceController: WKInterfaceController {
     @IBAction private func start() {
         
         print("Starting time")
-        self.timer.stop()
+        
+        self.timer.setDate(Date(timeIntervalSinceNow: self.duration))
+        self.timer.start()
+        
+        self.doneTimer = Timer.scheduledTimer(timeInterval: self.duration, target: self, selector: #selector(self.timerDone), userInfo: nil, repeats: false)
+    }
+    
+    @objc private func timerDone() {
+        print("done!!!")
     }
     
 }
